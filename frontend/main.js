ec39f243344a546ac25640ef72f337f151c5e6a2
xXPedrowXx <phlopes646@gmail.com>
2024-02-20 16:14:41 -0300

    ALL

---
+++ frontend/main.js
@@ -0,0 +1,13 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+import './assets/css/style.css';
+import Login from './modules/login';
+
+const login = new Login('.form-login');
+const cadastro = new Login('.form-cadastro');
+
+login.init();
+cadastro.init();
+
+// import './assets/css/style.css';
